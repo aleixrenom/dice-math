52118bcdad860a1429c26e54f8e5df0c40065d61
aleixrenom <aleixrenom@gmail.com>
2024-11-14 14:49:34 +0200

    History module start

--- src/modules/History.tsx
+++ src/modules/History.tsx
@@ -1,7 +1,39 @@
+interface CompletedEquationProps {
+  firstNum: number;
+  secondNum: number;
+  answer: number;
+  correct: boolean;
+}
+
+const CompletedEquation = ({
+  firstNum,
+  secondNum,
+  answer,
+  correct,
+}: CompletedEquationProps) => {
+  const equationColor = { color: correct ? "green" : "red" };
+
+  return (
+    <>
+      <span style={equationColor}>
+        {firstNum} + {secondNum} = {answer}
+      </span>
+    </>
+  );
+};
+
+const historyPlaceholder: CompletedEquationProps[] = [
+  { firstNum: 17, secondNum: 2, answer: 19, correct: true },
+  { firstNum: 17, secondNum: 2, answer: 23, correct: false },
+  { firstNum: 20, secondNum: 10, answer: 30, correct: true },
+];
+
 const History = () => {
   return (
     <div className="History-container">
-      <p>History</p>
+      {historyPlaceholder.map((eq, i) => (
+        <CompletedEquation key={i} {...eq} />
+      ))}
     </div>
   );
 };
